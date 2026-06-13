fbe8342f865d045898fda7e9a9ca9cae2bf771d8
aphexlog <aphexlog@gmail.com>
2026-06-13 15:30:29 -0500

    purple v0.1.1: fetch from tap repo release (public)

--- Formula/purple.rb
+++ Formula/purple.rb
@@ -5,11 +5,11 @@ class Purple < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://raw.githubusercontent.com/Elevator-Robot/homebrew-tap/main/releases/purple_0.1.1_darwin_amd64.tar.gz"
-      sha256 "e52874ce375856602def26fde162d744e13a3445b24729f8b535255758b5b277"
+      url "https://github.com/Elevator-Robot/homebrew-tap/releases/download/v0.1.1/purple_0.1.1_darwin_amd64.tar.gz"
+      sha256 "b4487f12f8bf0a7e79daeb7dfc71fde369bf39ba75254ee2430e2b813c6353a7"
     else
-      url "https://raw.githubusercontent.com/Elevator-Robot/homebrew-tap/main/releases/purple_0.1.1_darwin_arm64.tar.gz"
-      sha256 "7d7ba12347088d1597e8d5acc1e609f7ab1c7412aa92397d6cf9c02720721cda"
+      url "https://github.com/Elevator-Robot/homebrew-tap/releases/download/v0.1.1/purple_0.1.1_darwin_arm64.tar.gz"
+      sha256 "99cd60a80deb1c95992dfea18065cb65a064fd5aa52d16525732f19644fa9a75"
     end
   end
 
