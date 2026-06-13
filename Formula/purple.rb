a71a4deb54f152603c018ce49fbb3333f2736ca6
aphexlog <aphexlog@users.noreply.github.com>
2026-06-13 15:11:53 -0500

    purple v0.1.0 (binary release)

--- Formula/purple.rb
+++ Formula/purple.rb
@@ -1,19 +1,23 @@
 class Purple < Formula
   desc "Lightweight ngrok-style reverse proxy tunnel client"
   homepage "https://github.com/Elevator-Robot/purple"
-  url "https://github.com/Elevator-Robot/purple/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "626c7c545e7b7090d00a3b54625cfe35ea03a68b4adc4359d338f88c79a446ef"
   license "MIT"
 
-  depends_on "go" => :build
-
-  def install
-    cd("proxy/cmd/purple") do
-      system "go", "build", "-ldflags", "-X main.version=#{version}", "-o", bin/"purple"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/Elevator-Robot/purple/releases/download/v0.1.0/purple_0.1.0_darwin_amd64.tar.gz"
+      sha256 "90c343f5e46ee875e018575f23b7bf61778195de26b675f0d476f80bbc5e7d47"
+    else
+      url "https://github.com/Elevator-Robot/purple/releases/download/v0.1.0/purple_0.1.0_darwin_arm64.tar.gz"
+      sha256 "b068607d3bd0f94adaff1b4d28cf3820a182aa6fc287269b7db730a25c89120e"
     end
   end
 
+  def install
+    bin.install "purple"
+  end
+
   test do
-    assert_match "purple", shell_output("#{bin}/purple --help 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/purple --version")
   end
 end
